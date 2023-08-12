4e1456c5cc94693619f1c193474c22ac21642081
254jack <taixonj@gmail.com>
2023-08-12 09:12:16 +0300

    add

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,4 @@
 def print_list_integer(my_list=[]):
     if my_list:
         for i in my_list:
-            if i in range(48,58):
-                print("{}".format(i))
+            print("{}".format(i))
