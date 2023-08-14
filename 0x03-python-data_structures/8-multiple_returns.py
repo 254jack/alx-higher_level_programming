41622ce910293373af33f24597a39b357b514163
254jack <taixonj@gmail.com>
2023-08-14 11:09:20 +0300

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,9 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
+
     if length == 0:
         return None
     else:
         first = sentence[0]
-        return(length,first)
+        return(length, first)
