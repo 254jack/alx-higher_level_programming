e9f2fadd0a2592133cbf3950d4b98645e0fc81e1
254jack <taixonj@gmail.com>
2023-08-29 13:02:26 +0300

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,11 +8,12 @@ pre_square = __import__("0-square").Square
 class Square(pre_square):
     """ a class inherited from our previously defined square class
     Args:
-    	prev_square(class): a class that defines a square
+        prev_square(class): a class that defines a square
     """
+
     def __init__(self, size):
         """initialization function
         Args:
-        	size(integer): size attribute
+                size(integer): size attribute
             """
         self.__size = size
