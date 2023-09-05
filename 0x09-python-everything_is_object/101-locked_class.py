645ce35b7545c61762e4c2b74ed086e012d95fc2
254jack <taixonj@gmail.com>
2023-09-05 20:31:57 +0300

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """ a locked_class module"""
+
+
 class LockedClass:
-    """ a class named LockedClass"""
+    """ a class named LockedClass with no class or object attribute """
     __slots__ = ["first_name"]
+    """New instanse attribute"""
