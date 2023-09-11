75bdfe7efcec22299d27aeedd2d7d83138442d21
254jack <taixonj@gmail.com>
2023-09-11 12:48:29 +0300

    first commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-""" a function that returns a list of all attributes and methods inside a class"""
+""" a function that returns a list of available attributes and methods inside a class"""
 def lookup(obj):
 	return dir(obj)
