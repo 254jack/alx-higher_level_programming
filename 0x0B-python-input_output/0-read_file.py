9a372f3301cb64b83536f46b0e38b0e83abdb641
254jack <taixonj@gmail.com>
2023-09-12 12:18:46 +0300

    first commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,7 +3,10 @@
 
 
 def read_file(filename=""):
-    """ a function that reads a text file and prints it to stdout"""
+    """ a function that reads a text file and prints it to stdout
+    Args:
+    filename (str): name of file to be opened
+    """
     with open("UTF8", "r") as f:
         file = f.read()
         print(file, end="")
