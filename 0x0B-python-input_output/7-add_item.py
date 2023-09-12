acbb10c357e31f2c53b27a7fabb97dac6fb8f3de
254jack <taixonj@gmail.com>
2023-09-12 15:43:13 +0300

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" add_item module """
+from sys import argv
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+av_edit = argv[1:]
+
+try:
+    content_list = load_from_json_file("add_item.json")
+except:
+    content_list = []
+finally:
+    for arg in av_edit:
+        content_list.append(arg)
+    save_to_json_file(content_list, "add_item.json")
