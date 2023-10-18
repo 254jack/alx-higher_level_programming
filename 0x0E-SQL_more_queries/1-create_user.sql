c3f2e0e2983420bd3972aacaa908529a120faeba
254jack <taixonj@gmail.com>
2023-10-18 20:12:06 +0300

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
--- Crate user
--- Execute: cat 1-create_user.sql | mysql -hlocalhost -uroot -p
+-- Create user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
