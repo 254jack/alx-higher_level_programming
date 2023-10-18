2b1bdf7712acc71676b321b2559c96687d19ac25
254jack <taixonj@gmail.com>
2023-10-18 20:01:58 +0300

    0-list_database.sql

--- 0x0E-SQL_more_queries/0-list_database.sql
+++ 0x0E-SQL_more_queries/0-list_database.sql
@@ -1,2 +1,4 @@
--- This script list all databases in MySQL server
-SHOW DATABASES;
+-- Print all privileges of USER
+-- Execute: cat 0-privileges.sql | mysql -hlocalhost -uroot -p
+SHOW GRANTS FOR `user_0d_1`@`localhost`;
+SHOW GRANTS FOR `user_0d_2`@`localhost`;
