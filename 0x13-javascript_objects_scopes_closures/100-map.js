ce49a5c5fa32be1168b3984ea0622c448410063f
254jack <taixonj@gmail.com>
2023-11-14 13:21:15 +0300

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
 const originalList = require('./100-data').list;
+
+const mappedList = originalList.map((value, index) => value * index);
+
 console.log(originalList);
-const mappedList = originalList.map (function (e, index) {
-    return (e * index);
-});
 console.log(mappedList);
