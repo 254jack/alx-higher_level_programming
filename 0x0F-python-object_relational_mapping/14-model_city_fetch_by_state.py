bac58543c11782af341e0ec3a7907d23843db9da
254jack <taixonj@gmail.com>
2023-11-18 12:52:44 +0300

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,10 +10,10 @@ from model_state import State
 from model_city import City
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                         pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=eng)
     session = Session()
 
     for city, state in session.query(City, State) \
