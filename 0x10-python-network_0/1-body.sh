4fa41e9a84398e3b5a9be7705daf3af5e960428c
254jack <taixonj@gmail.com>
2023-11-30 13:31:06 +0300

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -Ls "$1"
+# Display only body of a 200 status code response
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
