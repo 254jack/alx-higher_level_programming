d0a0181bff61be2af52c02d355ebe6a0ef3b2055
254jack <taixonj@gmail.com>
2023-11-30 14:08:49 +0300

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-""" finding peak in a list of integers
-"""
-
+""" Finding a peak in a list of unsorted integers """
 
 def find_peak(list_of_integers):
-    """
-    a find peak method
-    """
-    pik = None
-    for i in list_of_integers:
-        if pik is None or pik < i:
-            pik = i
-    return pik
+    """ Find a peak using binary search """
+
+    if not list_of_integers:
+        return None
+
+    lw, hi = 0, len(list_of_integers) - 1
+
+    while lw < hi:
+        mid = (lw + hi) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            hi = mid
+        else:
+            lw = mid + 1
+
+    return list_of_integers[lw]
