d1da7800e713ae5c76f8290c38e3a9fcf0c018df
254jack <taixonj@gmail.com>
2023-11-30 12:48:50 +0300

    first commit

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Check if URL is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | cut -c1-10
