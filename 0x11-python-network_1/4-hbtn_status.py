b2070e87092c763875214cbaf74dfe124bd1fb90
254jack <taixonj@gmail.com>
2023-12-01 13:01:21 +0300

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
+import requests
+
+
+if __name__ == "__main__":
+    url = requests.get("https://alx-intranet.hbtn.io/status")
