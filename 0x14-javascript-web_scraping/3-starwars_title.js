35c6d84ae915d02d6d1c45510b80fb717d55c741
254jack <taixonj@gmail.com>
2024-01-03 21:39:00 +0300

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// javascript script that prints the title of a Star Wars movie
+//where episode is written in integer
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
